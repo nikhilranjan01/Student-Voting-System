ccd639c96b7220b017a87158ec841ce358181386
nikhilranjan01 <nikhilranjan2004@gmail.com>
2026-01-04 15:12:39 +0530

    New Update

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,9 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/nominees", nomineeRoutes);
 app.use("/api/votes", voteRoutes);
+app.get("/",(req,res)=>{
+  res.send("Hello Nikhil")
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
